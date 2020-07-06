55b88f7bf60546a3132f98cd26c07ec3e5b7d4c6
Olli <oliver.pfaff.email@gmail.com>
2020-07-06 13:45:04 +0200

    ADD TYPO3 support

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' => 
     array (
-      'typo3' => '7.5.0-8.99.99',
+      'typo3' => '7.5.0-9.5.99',
     ),
     'conflicts' => 
     array (
